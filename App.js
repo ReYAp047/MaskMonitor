2fdfe47621a4ba65ce20f9542235720b1552e264
Dali47 <meftah.dali@gmail.com>
2021-12-27 19:44:06 +0100

    FirstCommit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>This is my first expo application i wish that i finish it fast, i looking to finish all my university projects to have a rest</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  red: {
+    color: 'red',
+  },
+
 });
